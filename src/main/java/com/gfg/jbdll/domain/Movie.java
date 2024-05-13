efee276f6a30d1f6c50ebc7865c470efa4dcca07
Aman Kandari <kandariaman24@gmail.com>
2024-05-14 01:42:38 +0530

    Changed movie class to have release date field

--- src/main/java/com/gfg/jbdll/domain/Movie.java
+++ src/main/java/com/gfg/jbdll/domain/Movie.java
@@ -1,23 +1,28 @@
 package com.gfg.jbdll.domain;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 @Data
-
+@Builder
 public class Movie implements Serializable {
    private Integer id;
    private String title;
    private GENRE genre;
    private Double rating;
+   private LocalDate releaseDate;
 
-   public Movie(Integer id, String title, GENRE genre, Double rating) {
+   public Movie(Integer id, String title, GENRE genre, Double rating, LocalDate releaseDate) {
       this.id = id;
       this.title = title;
       this.genre = genre;
       this.rating = rating;
+      this.releaseDate = releaseDate;
    }
+
 
    protected boolean canEqual(final Object other) {
       return other instanceof Movie;
