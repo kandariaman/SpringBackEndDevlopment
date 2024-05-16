a971c3b78f3f288f1ebb6348d4360d612c83381d
Aman Kandari <kandariaman24@gmail.com>
2024-05-17 02:45:55 +0530

    Added code

--- src/main/java/com/gfg/jbdll/Firstspring52Application.java
+++ src/main/java/com/gfg/jbdll/Firstspring52Application.java
@@ -3,6 +3,7 @@ package com.gfg.jbdll;
 import com.gfg.jbdll.domain.GENRE;
 import com.gfg.jbdll.domain.Movie;
 import com.gfg.jbdll.domain.MyUser;
+import com.gfg.jbdll.service.MovieService;
 import com.gfg.jbdll.service.MyUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -26,6 +27,10 @@ public class Firstspring52Application implements CommandLineRunner {
 
 	@Autowired
 	RedisTemplate template;
+
+	@Autowired
+	MovieService movieService;
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(Firstspring52Application.class, args);
@@ -39,24 +44,25 @@ public class Firstspring52Application implements CommandLineRunner {
 		template.opsForValue().set("myJavaKey","myJavaValue");
 
 		String movieKey = "TopMovies";
-		Movie movie = new Movie(1, "MIB", GENRE.ACTION, 5.0);
-		template.opsForList().rightPush(movieKey, movie);
-
-		Movie movie1 = new Movie(2, "MIB-2", GENRE.ACTION, 5.0);
-		template.opsForList().rightPush(movieKey, movie1);
-
-		System.out.println(template.opsForList().range(movieKey, 0, -1));
-
-
-		template.opsForHash().put(movie.getTitle(), "title", movie.getTitle());
-		template.opsForHash().put(movie.getTitle(), "Id", movie.getId());
-		template.opsForHash().put(movie.getTitle(), "genre", movie.getGenre());
+//		Movie movie = new Movie(1, "MIB", GENRE.ACTION, 5.0);
+//		template.opsForList().rightPush(movieKey, movie);
+//
+//		Movie movie1 = new Movie(2, "MIB-2", GENRE.ACTION, 5.0);
+//		template.opsForList().rightPush(movieKey, movie1);
+//
+//		System.out.println(template.opsForList().range(movieKey,f 0, -1));
 
 
+//		template.opsForHash().put(movie.getTitle(), "title", movie.getTitle());
+//		template.opsForHash().put(movie.getTitle(), "Id", movie.getId());
+//		template.opsForHash().put(movie.getTitle(), "genre", movie.getGenre());
+//
 
 		System.out.println(template.opsForValue().get("myJavaKey"));
 		userDetailService.addUser(new MyUser("john", "123", "admin", "sha256"));
 		userDetailService.addUser(new MyUser("jenny","456","user", "sha256"));
+
+		movieService.getMoviesFromHomePage();
 	}
 }
 
